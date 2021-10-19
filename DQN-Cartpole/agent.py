23451dc8e59f1c4c12ec78d9c99f634a7cd82487
Mahmoud Akl <mahmoud.akl@gmail.com>
2021-10-19 10:30:48 +0200

    Added DQN CartPole example

---
+++ DQN-Cartpole/agent.py
@@ -0,0 +1,289 @@
+import sys
+import gym
+import torch
+import random
+import sunblaze_envs
+
+import numpy as np
+import torch.nn.functional as F
+
+from model import DSNN
+from collections import namedtuple, deque
+
+
+sys.path.append('../')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+QTensor = namedtuple('QTensor', ['tensor', 'scale', 'zero_point'])
+
+
+def quantize_tensor(x, min_val, max_val, qmin=-128, qmax=127):
+    scale = (max_val - min_val)/(qmax - qmin)
+
+    zero_point = 0
+    q_x = zero_point + (x/scale)
+    q_x.clamp(qmin, qmax).round_()
+    q_x = q_x.round().int()
+    return QTensor(tensor=q_x, scale=scale, zero_point=zero_point)
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward",
+                                                                "next_state", "done"])
+        self.seed = random.seed(seed)
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).\
+            float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).\
+            long().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).\
+            float().to(device)
+        next_states = torch.from_numpy(
+            np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).
+                                 astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+class Agent:
+    def __init__(self, env, policy_net, target_net, architecture, batch_size, memory_size, gamma,
+                 eps_start, eps_end, eps_decay, update_every, target_update_frequency, optimizer,
+                 learning_rate, num_episodes, max_steps, i_run, result_dir, seed, tau, SQN=False,
+                 random=False, quantization=False):
+        if random:
+            self.env = sunblaze_envs.make('SunblazeCartPoleRandomExtreme-v0')
+        else:
+            self.env = gym.make(env)
+        self.env.seed(seed)
+        self.env._max_episode_steps = max_steps
+        self.policy_net = policy_net
+        self.target_net = target_net
+
+        if quantization:
+            self.loihi_policy_net = DSNN(architecture, seed, self.policy_net.alpha,
+                                         self.policy_net.beta, self.policy_net.weight_scale,
+                                         batch_size, 64, self.policy_net.simulation_time)
+
+        self.architecture = architecture
+        self.batch_size = batch_size
+        self.memory_size = memory_size
+        self.gamma = gamma
+        self.eps_start = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+        self.update_every = update_every
+        self.target_update_frequency = target_update_frequency
+        self.optimizer = optimizer
+        self.learning_rate = learning_rate
+        self.num_episodes = num_episodes
+        self.max_steps = max_steps
+        self.i_run = i_run
+        self.result_dir = result_dir
+        self.seed = seed
+        self.tau = tau
+        self.SQN = SQN
+        self.random = random
+        self.quantization = quantization
+
+        # Initialize Replay Memory
+        self.memory = ReplayBuffer(self.memory_size, self.batch_size, self.seed)
+
+        # Initialize time step
+        self.t_step = 0
+        self.t_step_total = 0
+
+    def dequantize_tensor(self, q_x):
+        return q_x.scale * (q_x.tensor.float() - q_x.zero_point)
+
+    def quantize_weights(self, weights):
+        combined_weights = torch.cat([torch.flatten(w) for w in weights])
+        min_val = torch.min(combined_weights)
+        max_val = torch.max(combined_weights)
+        quantized_weights = []
+        for w in weights:
+            w = quantize_tensor(w, min_val, max_val, qmin=-256, qmax=254).tensor
+            odd_mask = (w%2 == 1)
+            w[odd_mask] -= 1
+            quantized_weights.append(w)
+
+        return quantized_weights
+
+    def select_action(self, state, eps=0.):
+        state = torch.from_numpy(state)
+        state = state.unsqueeze(0).to(device)
+        if random.random() > eps:
+            with torch.no_grad():
+                if self.SQN:
+                    if self.quantization:
+                        state = quantize_tensor(state, -4.5, 4.5).tensor.float()
+                        weights = self.policy_net.weights
+                        q_weights = [q_w.float() for q_w in self.quantize_weights(weights)]
+                        self.loihi_policy_net.weights = q_weights
+                        final_layer_values = self.loihi_policy_net.forward(state.float(), loihi=True)[0].cpu().data.numpy()
+                    else:
+                        final_layer_values = self.policy_net.forward(state.float())[0].cpu().data.numpy()
+
+                    return np.argmax(final_layer_values[0])
+                else:
+                    return np.argmax(self.policy_net.forward(state.float())[0].cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.architecture[-1]))
+
+    def step(self, state, action, reward, next_state, done):
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % self.update_every
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > self.batch_size:
+                experiences = self.memory.sample()
+                self.optimize_model(experiences)
+
+    def optimize_model(self, experiences):
+        states, actions, rewards, next_states, dones = experiences
+
+        # Get max predicted Q values (for next states) from target model
+        if self.SQN:
+            Q_targets_next = self.target_net.forward(next_states)[0].\
+                detach().max(1)[0].unsqueeze(1)
+                #gather(1, actions)
+        else:
+            Q_targets_next = self.target_net(next_states).detach().max(1)[0].unsqueeze(1)
+
+        # Compute Q targets for current states
+        Q_targets = rewards + (self.gamma * Q_targets_next*(1 - dones))
+
+        # Get expected Q values from local model
+        if self.SQN:
+            Q_expected = self.policy_net.forward(states)[0].gather(1, actions)
+        else:
+            Q_expected = self.policy_net.forward(states).gather(1, actions)
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        self.optimizer.step()
+        if self.t_step_total % self.target_update_frequency == 0:
+            self.soft_update()
+
+    def soft_update(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+    def train_agent(self):
+        best_average = -np.inf
+        best_average_after = np.inf
+        scores = []
+        smoothed_scores = []
+        scores_window = deque(maxlen=100)
+        eps = self.eps_start
+
+        for i_episode in range(1, self.num_episodes + 1):
+            state = self.env.reset()
+            score = 0
+            for t in range(self.max_steps):
+                self.t_step_total += 1
+                action = self.select_action(state, eps)
+                next_state, reward, done, _ = self.env.step(action)
+                self.step(state, action, reward, next_state, done)
+                state = next_state
+                score += reward
+                eps = max(self.eps_end, self.eps_decay * eps)
+                if done:
+                    break
+            scores_window.append(score)
+            scores.append(score)
+            smoothed_scores.append(np.mean(scores_window))
+
+            if smoothed_scores[-1] > best_average:
+                best_average = smoothed_scores[-1]
+                best_average_after = i_episode
+                if self.SQN:
+                    if self.quantization:
+                        torch.save(self.loihi_policy_net.state_dict(),
+                               self.result_dir + '/checkpoint_DSQN_Loihi_{}.pt'.format(self.i_run))
+                    else:
+                        torch.save(self.policy_net.state_dict(),
+                               self.result_dir + '/checkpoint_DSQN_{}.pt'.format(self.i_run))
+                else:
+                    torch.save(self.policy_net.state_dict(),
+                               self.result_dir + '/checkpoint_DQN_{}.pt'.format(self.i_run))
+
+            print("Episode {}\tAverage Score: {:.2f}\t Epsilon: {:.2f}".
+                  format(i_episode, np.mean(scores_window), eps), end='\r')
+
+            if i_episode % 100 == 0:
+                print("\rEpisode {}\tAverage Score: {:.2f}".
+                      format(i_episode, np.mean(scores_window)))
+
+        print('Best 100 episode average: ', best_average, ' reached at episode ',
+              best_average_after, '. Model saved in folder best.')
+        return smoothed_scores, scores, best_average_after
+
+
+def evaluate_agent(policy_net, env, num_episodes, max_steps, gym_seeds, epsilon=0,
+                   SQN=False, quantization=True):
+    """
+
+    """
+    rewards = []
+
+    for i_episode in range(num_episodes):
+        env.seed(int(gym_seeds[i_episode]))
+        env._max_episode_steps = max_steps
+        state = env.reset()
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        #if quantization:
+        #    state = quantize_tensor(state, -4.5, 4.5).tensor.float()
+        total_reward = 0
+        for t in range(max_steps):
+            if quantization:
+                state = quantize_tensor(state, -4.5, 4.5).tensor.float()
+            if random.random() >= epsilon:
+                if quantization:
+                    final_layer_values = policy_net.forward(state.float(), loihi=True)[0].cpu().data.numpy()
+                    action = np.argmax(final_layer_values[0])
+                else: 
+                    final_layer_values = policy_net.forward(state.float())[0].cpu().data.numpy()
+                    action = np.argmax(final_layer_values)
+            else:
+                action = random.randint(0, env.action_space.n - 1)
+
+            observation, reward, done, _ = env.step(action)
+            state = torch.from_numpy(observation).float().unsqueeze(0).to(device)
+            total_reward += reward
+            if done:
+                break
+        rewards.append(total_reward)
+        print("Episode: {}".format(i_episode), end='\r')
+
+    return rewards
---
+++ DQN-Cartpole/model.py
@@ -0,0 +1,219 @@
+import torch
+import random
+
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+from matplotlib.gridspec import GridSpec
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, architecture, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            architecture:
+        """
+        super(QNetwork, self).__init__()
+        self.layers = nn.ModuleList()
+        self.seed = torch.manual_seed(seed)
+        for i in range(len(architecture) - 1):
+            self.layers.append(nn.Linear(architecture[i], architecture[i + 1]))
+
+    def forward(self, x):
+        """Build a network that maps state -> action values."""
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+
+        # no ReLu activation in the output layer
+        return self.layers[-1](x)
+
+    def forward_return_all(self, x):
+        """
+        Same as the forward function but returns all intermediate results that are necessary for
+        conversion to a spiking network
+        """
+        all_neurons_output = []
+        for i in range(len(self.layers) - 1):
+            x = F.relu(self.layers[i](x))
+            all_neurons_output.append(x)
+
+        # no ReLu activation in the output layer
+        x = self.layers[-1](x)
+        all_neurons_output.append(x)
+        return all_neurons_output
+
+
+class SurrGradSpike(torch.autograd.Function):
+    """
+    Here we implement our spiking nonlinearity which also implements
+    the surrogate gradient. By subclassing torch.autograd.Function,
+    we will be able to use all of PyTorch's autograd functionality.
+    Here we use the normalized negative part of a fast sigmoid
+    as this was done in Zenke & Ganguli (2018).
+    """
+
+    scale = 100.0  # controls steepness of surrogate gradient
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we compute a step function of the input Tensor
+        and return it. ctx is a context object that we use to stash information which
+        we need to later backpropagate our error signals. To achieve this we use the
+        ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input)
+        out[input > 0] = 1.0
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor we need to compute the
+        surrogate gradient of the loss with respect to the input.
+        Here we use the normalized negative part of a fast sigmoid
+        as this was done in Zenke & Ganguli (2018).
+        """
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input / (SurrGradSpike.scale * torch.abs(input) + 1.0) ** 2
+        return grad
+
+
+class DSNN(nn.Module):
+    def __init__(self, architecture, seed, alpha, beta, batch_size, threshold, simulation_time):
+        """
+
+        """
+        self.architecture = architecture
+
+        self.seed = random.seed(seed)
+
+        self.alpha = alpha
+        self.beta = beta
+        self.threshold = threshold
+        self.simulation_time = simulation_time
+        self.batch_size = batch_size
+        self.spike_fn = SurrGradSpike.apply
+
+        # Initialize the network weights
+        self.weights = []
+        for i in range(len(architecture) - 1):
+            self.weights.append(torch.empty((self.architecture[i], self.architecture[i + 1]),
+                                            device=device, dtype=torch.float, requires_grad=True))
+            torch.nn.init.normal_(self.weights[i], mean=0.0, std=1)
+
+    def forward(self, inputs, loihi=False):
+        syn = []
+        mem = []
+        for l in range(0, len(self.weights)):
+            syn.append(torch.zeros((self.batch_size, self.weights[l].shape[1]), device=device,
+                                   dtype=torch.float))
+            mem.append(torch.zeros((self.batch_size, self.weights[l].shape[1]), device=device,
+                                   dtype=torch.float))
+
+        # Here we define two lists which we use to record the membrane potentials and output spikes
+        mem_rec = []
+        spk_rec = []
+
+        # Here we loop over time
+        for t in range(self.simulation_time):
+            # append the new timestep to mem_rec and spk_rec
+            mem_rec.append([])
+            spk_rec.append([])
+
+            if t == 0:
+                for l in range(len(self.weights)):
+                    mem_rec[-1].append(mem[l])
+                    spk_rec[-1].append(mem[l])
+                continue
+
+            # We take the input as it is, multiply is by the weights, and we inject the outcome
+            # as current in the neurons of the first hidden layer
+            input = inputs.detach().clone()
+
+            # loop over layers
+            for l in range(len(self.weights)):
+                if loihi:
+                    # define impulse
+                    if l == 0:
+                        h = torch.einsum("ab,bc->ac", [input, self.weights[0]])
+                    else:
+                        h = torch.einsum("ab,bc->ac", [spk_rec[-1][l - 1], self.weights[l]*64])
+                else:
+                    if l == 0:
+                        h = torch.einsum("ab,bc->ac", [input, self.weights[0]])
+                    else:
+                        h = torch.einsum("ab,bc->ac", [spk_rec[-1][l - 1], self.weights[l]])
+
+                # calculate the new synapse potential
+                if l == 0:
+                    new_syn = 0*syn[l] + h
+                else:
+                    new_syn = self.alpha*syn[l] + h
+
+                new_mem = self.beta*mem[l] + new_syn
+
+                # calculate the spikes for all layers but the last layer (decoding='potential')
+                if l < (len(self.weights) - 1):
+                    mthr = new_mem - self.threshold
+                    out = self.spike_fn(mthr).detach()
+                    c = (mthr > 0)
+                    new_mem[c] = 0
+                else:
+                    # else reset is 0 (= no reset)
+                    c = torch.zeros_like(new_mem, dtype=torch.bool, device=device)
+
+                mem[l] = new_mem
+                syn[l] = new_syn
+
+                mem_rec[-1].append(mem[l])
+                spk_rec[-1].append(out)
+
+        # return the final recorded membrane potential (len(mem_rec)-1) in the output layer (-1)
+        return mem_rec[-1][-1], mem_rec, spk_rec
+
+    def plot_voltage_traces(self, mem, spk=None, dim=(1, 1), spike_height=5):
+        gs = GridSpec(*dim)
+        if spk is not None:
+            dat = (mem + spike_height * spk).detach().cpu().numpy()
+        else:
+            dat = mem.detach().cpu().numpy()
+        for i in range(np.prod(dim)):
+            if i == 0:
+                a0 = ax = plt.subplot(gs[i])
+            else:
+                ax = plt.subplot(gs[i], sharey=a0)
+            ax.plot(dat[i])
+            ax.axis("off")
+
+    def load_state_dict(self, layers):
+        """Method to load weights and biases into the network"""
+        weights = layers[0]
+        #biases = layers[1]
+        for l in range(0,len(weights)):
+            self.weights[l] = weights[l].detach().clone().requires_grad_(True)
+
+    def state_dict(self):
+        """Method to copy the layers of the SQN. Makes explicit copies, no references."""
+        weights_copy = []
+        bias_copy = []
+        for l in range(0, len(self.weights)):
+            weights_copy.append(self.weights[l].detach().clone())
+        return weights_copy, bias_copy
+
+    def parameters(self):
+        parameters = []
+        for l in range(0, len(self.weights)):
+            parameters.append(self.weights[l])
+
+        return parameters
